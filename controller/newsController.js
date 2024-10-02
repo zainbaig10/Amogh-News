ffe6b75381329981c26dd9d0b6653994883b6a7d
[zain baig] <[zain.baig98gmail.com]>
2024-10-02 15:57:26 +0530

    get apis

--- controller/newsController.js
+++ controller/newsController.js
@@ -41,3 +41,53 @@ export const addNews = asyncHandler(async (req, res) => {
     return res.status(500).json({ success: false, error });
   }
 });
+
+export const getAllNews = asyncHandler(async (req, res) => {
+  try {
+    const news = await News.find({}).exec(); 
+    return res.status(200).json({ news, success: true });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Failed to fetch news", success: false });
+  }
+});
+
+export const getAllNewsWithPagination = asyncHandler(async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1; // Default page is 1
+    const pageSize = parseInt(req.query.pageSize) || 10; // Default pageSize is 10
+    const sortField = req.query.sortField || "dateCreated"; // Default sort field
+    const sortOrder = req.query.sortOrder || "desc"; // Default sort order
+    const sort = {};
+    sort[sortField] = sortOrder === "asc" ? 1 : -1; // Ascending or Descending
+    const startIndex = (page - 1) * pageSize;
+
+    // Get total count of news documents
+    const totalDocuments = await News.countDocuments();
+    const totalPages = Math.ceil(totalDocuments / pageSize);
+
+    // Fetch paginated news articles
+    const news = await News.find({})
+      .sort(sort)
+      .skip(startIndex)
+      .limit(pageSize)
+      .exec();
+
+    // Response
+    return res.status(200).json({
+      news,
+      pagination: {
+        page,
+        pageSize,
+        totalPages,
+        totalDocuments,
+        hasNextPage: page < totalPages,
+        hasPrevPage: page > 1,
+      },
+      success: true,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message, success: false });
+  }
+});
